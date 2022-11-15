7345765b1848acf861a8d83b8c0fea865cdaec9d
matibuet91 <110296076+matibuet91@users.noreply.github.com>
2022-11-14 21:21:23 -0300

    14/11/2022

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
 const API = "https://api.github.com/users/";
+
+
+const apps = Vue.createApp({
+    data(){
+        return {
+            busqueda: "",
+            result:null,
+            error:null,
+            favoritos: new Map(),
+        }
+    },
+    computed: {
+        esFavorito(){
+            return this.favoritos.has(this.result.login)
+        },
+        misFavoritos(){
+            let resp = Array.from(this.favoritos.values());
+            return resp;
+        }
+    },
+    methods: {
+        search(){
+            const self = this;
+            this.result = this.error = null;
+           
+            $.ajax({
+                url: API + self.busqueda,
+                method: 'GET',
+            }).done(function (data){
+                self.result = data;
+            }).fail (function (){
+                self.error = "No se encontro el usuario"
+            })
+        },
+        aumentarValor(){
+           this.contador++;
+        },
+        addFavoritos(){
+            this.favoritos.set(this.result.login, this.result);
+        },
+        quitarFavorito(){
+            this.favoritos.delete(this.result.login);
+        }
+    }
+})
