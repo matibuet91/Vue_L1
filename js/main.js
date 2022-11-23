4e28185f3b9cc89f8aa83af1c74ef832b04f7fb3
matibuet91 <110296076+matibuet91@users.noreply.github.com>
2022-11-23 19:21:02 -0300

    Clase 44

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ const apps = Vue.createApp({
             result:null,
             error:null,
             favoritos: new Map(),
+            pruebas: [
+                {key:0, name:"mati"}, 
+                {key:1, name:'otro'}
+            ],
+            numeros:[
+                1,2,3,4,6,7
+            ]
         }
     },
     computed: {
@@ -38,9 +45,41 @@ const apps = Vue.createApp({
         },
         addFavoritos(){
             this.favoritos.set(this.result.login, this.result);
+            this.actualizarStorage();
         },
         quitarFavorito(){
             this.favoritos.delete(this.result.login);
+            this.actualizarStorage();
+        },
+        actualizarStorage(){
+            localStorage.setItem('favoritos', JSON.stringify(this.misFavoritos));
+        },
+        mostrarFavorito(favorito){
+            this.result = favorito;
+
         }
+
+    },
+    beforeCreate(){
+        console.log('beforeCreate');
+    },
+    created(){
+       const favoritosGuardados = JSON.parse(localStorage.getItem('favoritos'));       
+       if (favoritosGuardados.length){
+            var favoritos = new Map(favoritosGuardados.map(fav => [fav.login, fav]))
+            this.favoritos = favoritos;
+       }
+    },
+    beforeMount(){
+        console.log('beforeMount');
+    },
+    mounted(){
+        console.log('mounted');
+        var items =  $('#tt').html();
+        for (let index = 0; index < this.pruebas.length; index++) {  
+            items = items + '<li>' + this.pruebas[index].name  + '</li>'
+        }
+        $('#tt').html(items);
+
     }
 })
